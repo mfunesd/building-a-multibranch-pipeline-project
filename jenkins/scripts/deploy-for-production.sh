5ea77ffdebb839a046c3c0ea62c5efee1851a7ed
mfunesd <fdmanuel@yahoo.es>
2019-05-01 13:45:09 +0200

    Modified deploy-for-production.sh and Jenkinsfile for Production

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -34,6 +34,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:5100 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 5100:5000''" parameter when you'
+echo 'Visit http://localhost:5080 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 5080:5000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
